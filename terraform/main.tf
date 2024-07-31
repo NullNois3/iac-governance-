00a2572365588aac11af940786b9652609e86645
NullNois3 <59552532+NullNois3@users.noreply.github.com>
2024-07-30 23:11:36 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "test_role" {
   name = "null-atest_role"
 
-  # Terraform's "jsonencode" function converts a
+  # Terraform's "jsonencode" function converts a -
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
